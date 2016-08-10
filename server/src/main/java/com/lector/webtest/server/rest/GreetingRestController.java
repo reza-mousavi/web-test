31faf72e8fc11800c07dda7b2a973e945af25f20
rezamda <reza.mousavi@lector.dk>
2016-08-10 16:36:56 +0200

    Handling conflict issues

--- server/src/main/java/com/lector/webtest/server/rest/GreetingRestController.java
+++ server/src/main/java/com/lector/webtest/server/rest/GreetingRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingRestController {
 
-    @RequestMapping(value = "/rest/greeting", produces = "application/json")
+    @RequestMapping(value = "/rest/greeting", produces = "application/json;charset=UTF-8")
     public String greet(
             @RequestParam(name = "name", defaultValue = "World")String name
     ){
